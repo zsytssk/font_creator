659811b287739dec36fa11ae19c4d1499805651e
zsytssk <zsytssk@gmail.com>
2021-11-24 14:40:52 +0800

    test/1122_update:

--- lib/main.dart
+++ lib/main.dart
@@ -3,8 +3,10 @@ import 'package:flutter/material.dart';
 import 'package:font_creator/home/home_model.dart';
 import 'package:provider/provider.dart';
 import 'package:font_creator/home/home.dart';
+import 'configure_nonweb.dart' if (dart.library.html) 'configure_web.dart';
 
 main() async {
+  configureApp();
   runApp(MultiProvider(
       providers: [
         ChangeNotifierProvider(create: (_) => HomeNotifier(HomeData())),
